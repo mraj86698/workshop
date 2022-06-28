136611b4287e1c53901a135dce9e802cf64187c5
MohanRaj <mraj86698@gmail.com>
2022-06-28 20:08:41 +0530

    Added Gambling for the 20 days

--- src/gimplingPgme/GimblingSimulator.java
+++ src/gimplingPgme/GimblingSimulator.java
@@ -6,28 +6,21 @@ public class GimblingSimulator {
 
 	public static void main(String[] args) {
 
-		int stake = 100;
-		int bet = 1;
-		int totalAmountAfterBetting,stakeincrease = 1, maxStake = 150, minStake = 50;;
-
-		if(stake > maxStake && stake < minStake) {
-//			double betting=Math.random();
-			float betting = (float) Math.random()%2;
-			if(betting <=0.5) {
-				System.out.println("gambler won the bets");
-				totalAmountAfterBetting=stake+bet;
-				System.out.println("amount is" +" "+ totalAmountAfterBetting);
-				System.out.println("The player has retired with "+stake+" for the day");
+		int stake = 100, bet = 1, stakeincrease = 1, maxStake = 150, minStake = 50;
+		for(int day =1;day<=20;day++) {
+			
+			float random = (float) Math.random()%2;
+			if( random >= 0.5){
+				System.out.println("Win!!!");
+				stake = 150;
 			}
-			else {
-				System.out.println("gambler lost the bets");
-				totalAmountAfterBetting=stake-bet;
-				System.out.println("amount is"+" "+totalAmountAfterBetting);
+			else if( random < 0.5) {
+				System.out.println("Loose!!!");
+				stake =50;	
 			}
-
-
-
-		}
+			
+		    System.out.println("Total stakes for Day "+ day +" : "+ stake);
+	    }
 
 	}
 }
